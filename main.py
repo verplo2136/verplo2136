564732bc2bdd181133e7fbb564ef1ae504c90da2
verplo2136 <137895676+verplo2136@users.noreply.github.com>
2023-08-16 13:05:47 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+from hello import hello
+
+def main():
+    name = "Your Veronika"  # Replace this with your actual name
+    hello(name)
+
+if __name__ == "__main__":
+    main()
---
+++ numpy_example.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# kod z numpy
+a = np.array([1, 2, 3])
+b = np.array([4, 5, 6])
+c = a + b
+
+print("Array a:", a)
+print("Array b:", b)
+print("Sum array c:", c)
